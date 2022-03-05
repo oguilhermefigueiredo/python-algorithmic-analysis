6933cc9f492fc345eca0791df075e758ee33e8f5
Guilherme <negocio.guilhermefigueiredo@gmail.com>
2022-03-05 18:52:31 -0300

    Linear searching

---
+++ linear-search.py
@@ -0,0 +1,18 @@
+arr = []
+
+def f(n):
+    a, b = 0, 1
+
+    for number in range(1, n):
+        a, b = b, b + a
+        arr.append(b)
+
+def search(arr, target):
+    for number in arr:
+        if number == target:
+            return number
+
+    return -1
+
+f(12)
+print(search(arr, 144))
