62fb036d12912c2ac0e3dbfdd58166e0a09b3628
Guilherme <negocio.guilhermefigueiredo@gmail.com>
2022-03-06 15:23:41 -0300

    Improved array insert algorithm into just 2 lines

--- array-insertion.py
+++ array-insertion.py
@@ -3,9 +3,13 @@ arr = [1, 2, 3, 5]
 
 def insert(arr, index, successor):
     for index in range(index, len(arr)):
-        arr[index], successor = successor, arr[index]
+        (arr[index], successor) = (successor, arr.insert(index, successor))
 
-    arr.append(successor)
+# def insert(arr, index, successor):
+#     for index in range(index, len(arr)):
+#     arr[4] = successor
+#     arr.append(successor)
+#     arr.insert(4, successor)
 
 
 print('Array before insertion:', arr)
