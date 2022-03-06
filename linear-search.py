8eb1eba0415f288eaa433efff4f78e252cff4baf
Guilherme Figueirêdo <97318219+oguilhermefigueiredo@users.noreply.github.com>
2022-03-06 16:28:26 -0300

    Update linear-search.py

--- linear-search.py
+++ linear-search.py
@@ -1,13 +1,6 @@
 
 arr = [1, 2, 3, 4, 5, 5, 5, 1, 2]
 
-def f(n):
-    a, b = 0, 1
-
-    for number in range(1, n):
-        a, b = b, b + a
-        arr.append(b)
-
 def search(arr, target):
     if target in arr:
         return target
