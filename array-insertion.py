76ad52391d725f6ed3583a3db468d3ff4964e723
Guilherme <negocio.guilhermefigueiredo@gmail.com>
2022-03-07 08:48:51 -0300

    Algorithm performing an array insertion

--- array-insertion.py
+++ array-insertion.py
@@ -4,14 +4,8 @@ arr = [1, 2, 3, 5]
 def insert(arr, index, successor):
     for index in range(index, len(arr)):
         (arr[index], successor) = (successor, arr.insert(index, successor))
-
-# def insert(arr, index, successor):
-#     for index in range(index, len(arr)):
-#     arr[4] = successor
-#     arr.append(successor)
-#     arr.insert(4, successor)
-
+        break
 
 print('Array before insertion:', arr)
-insert(arr, 3, -4)
+insert(arr, 1, -4)
 print('Array after insertion:', arr)
