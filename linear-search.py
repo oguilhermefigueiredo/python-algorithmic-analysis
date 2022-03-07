f3f65c4f2cff92c923495bc8582eca3412ab81fb
Guilherme <negocio.guilhermefigueiredo@gmail.com>
2022-03-07 09:23:27 -0300

    Linear searching with loops, with just one branching

--- linear-search.py
+++ linear-search.py
@@ -1,4 +1,10 @@
 
+# Linear searching using just one ```if``` statement.
+# Before I improve this code, a ```for``` loop was needed to go through every
+# single item in the list, with a codition comparing the item with the target.
+# With my solution, you just need one block of code the get the result, being
+# more than 70% faster than the precedent solution.
+
 arr = [1, 2, 3, 4, 5, 5, 5, 1, 2]
 
 def search(arr, target):
